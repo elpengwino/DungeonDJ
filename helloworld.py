6fb6dff073358987c3b675714d0ec4137399bc1e
elpengwino <gabe.resneck@gmail.com>
2020-06-27 20:41:40 -0700

    Hello world - 1: Added PyQt elements

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+app = QApplication([])
+window = QWidget()
+layout = QVBoxLayout()
+layout.addWidget(QPushButton('Play'))
+window.setLayout(layout)
+window.show()
+app.exec_()
